70b9cfafb89809c732fef3f97e9201f60f37f8bc
WinstonKamau <29925144+WinstonKamau@users.noreply.github.com>
2019-01-23 18:37:22 +0300

    feat(migration): add models to main models file
    - add models to the main models file
    - push migrations
    - remove migrations folder from coverage
    
    [ Fixes #163422006]

---
+++ src/logogram/logogram_v1/models.py
@@ -0,0 +1,4 @@
+from logogram_v1.domain_persistence.users.models import Users
+from logogram_v1.domain_persistence.jargons.models import Jargons
+from logogram_v1.domain_persistence.flashcards.models import FlashCards
+from logogram_v1.domain_persistence.words.models import Words
