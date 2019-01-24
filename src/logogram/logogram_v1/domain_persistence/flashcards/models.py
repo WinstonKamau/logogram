016270b8d1ad2d7229831cbaae9fb88f0e560dd6
WinstonKamau <29925144+WinstonKamau@users.noreply.github.com>
2019-01-24 14:31:03 +0300

    chore(jargons): remove jargons
    - strip down application needs
    
    [ Fixes #163451653]

--- src/logogram/logogram_v1/domain_persistence/flashcards/models.py
+++ src/logogram/logogram_v1/domain_persistence/flashcards/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from logogram_v1.domain_persistence.jargons.models import Jargons
 from logogram_v1.domain_persistence.users.models import Users
 from django.utils import timezone
 
@@ -12,9 +11,4 @@ class FlashCards(models.Model):
                                          default=timezone.now)
     modification_date = models.DateTimeField(_('date_modified'),
                                              auto_now_add=True)
-    privacy = models.BooleanField(
-        _('privacy status'), default=True, help_text=_(
-         'Designates whether the flashcards are private or public.'),)
-    jargon = models.ForeignKey(Jargons, on_delete=models.SET_NULL, null=True,
-                               blank=True)
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
