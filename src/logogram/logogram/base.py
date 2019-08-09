6d48acb94bd362000a8a3ce9600a3b54de0b57b9
WinstonKamau <29925144+WinstonKamau@users.noreply.github.com>
2019-08-10 01:18:51 +0300

    feat(security): production environment
    * setup the deployed application for a production environment
    * provide both development and staging environments.
    
    [ Fixes #167819957 ]

--- src/logogram/logogram/base.py
+++ src/logogram/logogram/base.py
@@ -19,14 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mg2vjvbdgr9^9-g9yx$-jvm2jv2xd4ma2c2slv&shoo0hg!8-z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
---
+++ src/logogram/logogram/development.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'mg2vjvbdgr9^9-g9yx$-jvm2jv2xd4ma2c2slv&shoo0hg!8-z'
+
+ALLOWED_HOSTS = []
