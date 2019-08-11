eb0f59ea5037820f76e69d7cd7ffda8c45872002
WinstonKamau <29925144+WinstonKamau@users.noreply.github.com>
2019-08-11 21:13:15 +0300

    add versions for cookbook

--- devops/chef-setup/chef-cookbooks/logogram-os/recipes/default.rb
+++ devops/chef-setup/chef-cookbooks/logogram-os/recipes/default.rb
@@ -5,14 +5,13 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 apt_update 'Update the apt cache on chef client runs' do
-    action :update
-  end
-package 'python3-pip' do
-    version '' 
+  action :update
 end
+
 package 'nginx' do
-    version ''
+  version '1.10.*'
 end
+
 package 'supervisor' do
-    version ''
+  version '3.2.*'
 end
